4c34af1907390e5c79c1b511a98c89069753a124
Matthias <info@matthias-andrasch.de>
2019-08-18 16:53:27 +0200

    Update welcome.php

--- web/application/views/oerhoernchen/welcome.php
+++ web/application/views/oerhoernchen/welcome.php
@@ -4,7 +4,9 @@
         <div class="row">
         	<div class="mx-auto col-lg-9 col-sm-12">
 
-            Dies ist die Demo-Instanz für zukünftige Funktionen, die ggf. auf www.oerhoernchen.de hinzugefügt werden sollen. Feedback kann gerne an info (ät) matthias-andrasch.de gesendet werden. Viel Spaß beim Ausprobieren!
+            <p>Dies ist die Demo-Instanz für zukünftige Funktionen, die ggf. auf www.oerhoernchen.de hinzugefügt werden sollen. Feedback kann gerne an info (ät) matthias-andrasch.de gesendet werden. Viel Spaß beim Ausprobieren!</p>
+
+            <p>Aktuelle Demo: <a href="<?php echo site_url('hochschule/');?>">Hochschul-OER-Suche</a></p>
 
 			    </div>
 		</div>
