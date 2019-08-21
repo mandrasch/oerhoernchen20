fa2696337cb21b2cad8595cefa04da51eb98d4d5
Matthias <info@matthias-andrasch.de>
2019-08-21 11:01:33 +0200

    Update about_add_entry.php

--- web/application/views/oerhoernchen/about_add_entry.php
+++ web/application/views/oerhoernchen/about_add_entry.php
@@ -4,28 +4,30 @@
         <div class="row">
         	<div class="mx-auto col-lg-9 col-sm-12" style="font-size:18px;">
 
-
-
-            <p><b>Sie möchten einen bereits hochgeladenen Inhalt zum Suchindex hinzufügen?</b> <br> Der einfachste Weg wäre, hier ein Formular zur Erfassung anzubieten. Dieses habe ich bereits prototypisch implementiert: <a href="<?php echo site_url("lesezeichen/hinzufuegen");?>">Eintrag hinzufügen</a>. Als Einzelperson kann ich aber die Verantwortung für die Redaktion sowie die Haftungsrisiken für mögliche Urheberrechtsfragen nicht tragen, sodass diese Funktion nicht öffentlich aktiviert ist. Daher sind Bundesländerinitiativen sowie Hochschulangebote so immens wichtig für den offenen Austausch von Lehrmaterial: Es geht um Verantwortung und langfristiges Bereitstellen der Inhalte.
+            <p> Disclaimer: Es handelt sich bei dieser Webseite um einen Prototypen.</p>
+            <h2>Sie möchten einen bereits hochgeladenen Inhalt zum Suchindex hinzufügen?</h2>
+            <p>Der einfachste Weg wäre, hier ein Formular zur Erfassung anzubieten. Dieses habe ich bereits prototypisch implementiert: <a href="<?php echo site_url("lesezeichen/hinzufuegen");?>">Eintrag hinzufügen</a>. Als Einzelperson kann ich aber die Verantwortung für die Redaktion sowie die Haftungsrisiken für mögliche Urheberrechtsfragen nicht tragen, sodass diese Funktion nicht öffentlich aktiviert ist. Daher sind Bundesländerinitiativen sowie Hochschulangebote so immens wichtig für den offenen Austausch von Lehrmaterial: Es geht um Verantwortung und langfristiges Bereitstellen der Inhalte.
               <p>Je nach Bundeslandzugehörigkeit haben Sie  bereits einige Optionen auf Bundeslandebene - in Zukunft sollen durch OER-Länderinitiativen weitere Möglichkeiten hinzukommen (ggf. existiert an ihrer Hochschule bereits ein OER-Angebot/Repository, diese sind jedoch noch nicht sehr verbreitet). Das große Ziel ist hierbei, dass in allen 16 Bundesländern Hochschulmitarbeiter*innen offenes Lehrmaterial in vielfältigen Formaten veröffentlichen können, welches dann über gute Suchinterfaces einfach und schnell auffindbar ist (dezentrale Vernetzung). Derzeitige Optionen:
             <ul>
               <li><a href="#fueralle">Für alle Wissenschaftler*innen</a></li>
-              <li><a href="#bawue">Hochschul-Mitarbeiter*innen aus Baden Württemberg</a></li>
-              <li><a href="#hoou">Hochschul-Mitarbeiter*innen aus Hamburg</a></li>
-              <li><a href="#nrw">(zukünftig) Hochschul-Mitarbeiter*innen aus NRW</a></li>
-              <li><a href="#hessen">(zukünftig) Hochschul-Mitarbeiter*innen aus Hessen</a></li>
-              <li><a href="#niedersachsen">(zukünftig) Hochschul-Mitarbeiter*innen aus Niedersachen</a></li>
+              <li><a href="#bawue" style="color:darkgreen;">Hochschul-Mitarbeiter*innen in Baden Württemberg</a></li>
+              <li><a href="#hoou">Hochschul-Mitarbeiter*innen in Hamburg</a></li>
+                <li><a href="#rlp">(zukünftig) Hochschul-Mitarbeiter*innen in Rheinland-Pfalz</a></li>
+              <li><a href="#nrw">(zukünftig) Hochschul-Mitarbeiter*innen in NRW</a></li>
+              <li><a href="#niedersachsen">(zukünftig) Hochschul-Mitarbeiter*innen in Niedersachen</a></li>
+                <li><a href="#hessen">(zukünftig) Hochschul-Mitarbeiter*innen in Hessen</a></li>
+                <li><a href="#oesterreich">Hochschulmitarbeiter*in in Österreich</a></li>
             </ul>
           </p>
 
-            <h2 id="fueralle">Für alle</h2>
+            <h2 id="fueralle">Für alle Wissenschaftler*innen</h2>
 
               <p>Generell haben Sie, unabhängig vom Bundesland und ob Sie derzeit im Anstellungsverhältnis an einer Hochschule sind, folgende Optionen: Für Dokumente und Präsentationen bietet sich <a href="https://zenodo.org">Zenodo</a> an, für wissenschaftliche Videos das <a href="https://av.tib.eu/">TIB AV Portal</a>, interaktive h5p-Inhalte können erstmal auf der Demo-Instanz <a href="https://h5p.org/">h5p.org</a> veröffentlicht werden und statische Webseiten können auf <a href="https://gitlab.com">Gitlab.com</a> (GitLab Pages) gehostet werden.</p>
 
 
-            <h2 id="bawue">Baden Württemberg (alle)</h2>
+            <h2 id="bawue" style="color:darkgreen;">Baden Württemberg</h2>
 </
-            <p>Dank des Teams an der Uni Tübingen (Projektleitung: Peter Rempis) sind Sie in der glücklichen Lage, als derzeit einziges Bundesland ihre OER-Inhalte bereits hochladen und indexieren zu können mittels einer Web-Oberfläche.<br>Melden Sie sich in der <a href="https://www.oerbw.de/edu-sharing/components/search?mainnav=true">ZOERR-Weboberfläche</a> mit Ihrem Uni-Login an:</p>
+            <p>Dank des Teams an der Uni Tübingen (Peter Rempis, Michael Menzel und weitere hochengagierte Mitarbeiter*innen) sind Sie als Hochschulmitarbeiter*in in der glücklichen Lage, als derzeit einziges Bundesland ihre OER-Inhalte bereits hochladen und indexieren zu können mittels einer Web-Oberfläche.<br>Melden Sie sich in der <a href="https://www.oerbw.de/edu-sharing/components/search?mainnav=true">ZOERR-Weboberfläche</a> einfach mit Ihrem Uni-Login an:</p>
 
             <img src="<?php echo base_url();?>/assets/img/zoerr_login.png" style="max-width:100%;"/>
 
@@ -35,28 +37,40 @@
 
           <p>  <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/SHB7Tpc8UAc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
 
+
             <h2 id="hoou">Hamburg (Förderprojekte)</h2>
 
-              <p>  Auf dem Portal der <a href="https://www.hoou.de/">HOOU</a> (Hamburg Open Online University) stellen derzeit nur die OER-Förderprojekte ihr Material ein. Ob das Portal für alle Lehrenden geöffnet wird, kann ich derzeit nicht einschätzen - als engagierter Lehrende/r oder wissenschaftliche Mitarbeiter/innen können sie aber sicher jederzeit mal beim <a href="https://www.hoou.de/p/kontakt/">HOOU-Team</a> anklopfen und Ihr Interesse signalisieren.</p>
+              <p>Auf dem Portal der <a href="https://www.hoou.de/">HOOU</a> (Hamburg Open Online University) stellen derzeit die HOOU-Förderprojekte ihr Material ein. Ob das Portal für alle Lehrenden geöffnet wird, kann ich derzeit nicht einschätzen - als engagierter Lehrende/r oder wissenschaftliche Mitarbeiter/innen können sie aber sicher jederzeit mal beim <a href="https://www.hoou.de/p/kontakt/">HOOU-Team</a> anklopfen und Ihr Interesse signalisieren.</p>
+
+              <p>Tolle Sache: Auch externe Personen können HOOU-Projektförderungen erhalten, siehe Infos <a href="https://www.hoou.de/blog/foerdermoeglichkeiten-fuer-hoou-kooperationsprojekte">hier</a>
 
               <p>Als Mitarbeiter*in der Uni Hamburg haben Sie derzeit einen kleinen Wettbewerbsvorteil: Da die Uni Hamburg sich via DFN-AAI mit OERBW verbunden hat, können auch Sie über die obige Baden-Württemberg-Weboberfläche Inhalte hinzufügen.</p>
 
+              <h2 id="rlp">(zukünftig) Rheinland-Pfalz</h2>
+                <p>In Rheinland-Pfalz arbeiten engagierte Menschen bereits seit längerer Zeit am offenen intausch von Lehrmaterial, siehe z.B. <a href="https://www.oer-at-rlp.de">OER@RLP</a>. Eine Veröffentlichungsmöglichkeit ist derzeit in der Beta-Phase, bei Fragen kann man sich vertrauensvoll ans <a href="https://www.vcrp.de/">VCRP</a> wenden.
+
             <h2 id="nrw">(zukünftig) NRW</h2>
-              <p>In NRW laufen derzeit Vorprojekte, Portal als auch dezentrale Lösungen sind im Gespräch: <a href="http://heureka.blogs.ruhr-uni-bochum.de">Projektblog und Infos</a></p>
+              <p>In NRW laufen derzeit Vorprojekte, ein Portal als auch dezentrale Lösungen für die bestehende Hochschulinfrastruktur (LMS,Repositorien) sind im Gespräch: <a href="http://heureka.blogs.ruhr-uni-bochum.de">Projektblog und Infos</a>.</p>
+
+              <p>Eine Liste von bereits aktiven OER-Projekten in NRW findet sich <a href="https://heureka.blogs.ruhr-uni-bochum.de/workshop-oernrw-oer-projekte-an-hochschulen-in-nrw-09-07-2019/">hier</a></p>
+
+              <p>Aktuelle Info: Bis 30. November 2019 bewerben: <a href="https://www.mkw.nrw/foerderlinien-digitalisierungsoffensive">Content-Förderlinie OERcontentNRW sowie Förderlinie „Netzwerk Landesportal DH-NRW“ (OER-Bezug)</a></p>
 
               <p>Als Mitarbeiter*in der Uni Köln haben Sie derzeit einen kleinen Wettbewerbsvorteil: Da die Uni Köln sich via DFN-AAI bereits mit OERBW verbunden hat, können auch Sie über die obige Baden-Württemberg-Weboberfläche Inhalte hinzufügen.</p>
-
-            <h2 id="nrw">(zukünftig) Hessen</h2>
-              <p>Auch in Hessen beschäftigt man sich mit OER, siehe <a href="https://www.digll-hessen.de/de">Digital gestütztes Lehren und Lernen in Hessen</a></p>
 
             <h2 id="niedersachsen">(zukünftig) Niedersachsen</h2>
               <p>In Niedersachen startet in Kürze das Projekt „Open Educational Resources (OER) Portal Niedersachsen“, beteiligt ist u.a. die TIB.</p>
 
-            <h2 id="rlp">RLP</h2>
-              <p>Infos folgen</p>
+              <h2 id="nrw">(zukünftig) Hessen</h2>
+                <p>Auch in Hessen beschäftigt man sich mit OER, siehe <a href="https://www.digll-hessen.de/de">Digital gestütztes Lehren und Lernen in Hessen</a></p>
 
-              <p>Bundesland fehlt? >> info ((at)) matthias-andrasch.de</p>
+                <h2 id="oesterreich">Österreich</h2>
 
+                <p><a href="https://www.openeducation.at/home/">https://www.openeducation.at/home/</a></p>
+
+                <h2>Bundesland/Land fehlt?</h2>
+
+              <p>Bundesland fehlt? >> info ((at)) matthias-andrasch.de oder <a href="https://twitter.com/m_andrasch">Twitter-DM</a></p>
 			    </div>
 		</div>
 	</div>
